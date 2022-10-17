baea2bf2d6431c1e923de3b7d5041d03836a8f92
bluepnwage <a.carty2555@gmail.com>
2022-10-17 20:08:18 -0300

    update mantine and next

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    images: {
-      allowFutureImage: true
-    }
-  },
+
   async headers() {
     return [
       {
