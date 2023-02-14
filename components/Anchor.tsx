64e3ad4ee3d68551f1762251a0aeaaa14f92ad38
bluepnwage <a.carty2555@gmail.com>
2023-02-14 13:21:13 -0400

    change color theme

--- components/Anchor.tsx
+++ components/Anchor.tsx
@@ -4,7 +4,7 @@ type PropTypes = ComponentPropsWithoutRef<"a">;
 
 export function Anchor({ children, ...anchorProps }: PropTypes) {
   return (
-    <a {...anchorProps} target={"_blank"} className="dark:text-primary-dark text-primary hover:underline">
+    <a {...anchorProps} target={"_blank"} className="dark:text-primary-70 text-primary-40 hover:underline">
       {children}
     </a>
   );
--- components/bio/Bio.tsx
+++ components/bio/Bio.tsx
@@ -7,12 +7,16 @@ export function Bio() {
   return (
     <>
       <header className="flex flex-col items-center">
-        <h1 className="font-bold text-center md:text-start w-4/5 md:w-3/5 text-5xl mb-10 md:mb-5">Agis Carty</h1>
+        <h1 className="font-bold text-center md:text-start w-4/5 md:w-3/5 text-5xl mb-10 md:mb-5">
+          Agis Carty
+        </h1>
       </header>
       <section id="about" className="mb-20 flex flex-col items-center">
         <div className="flex md:flex-row w-4/5 md:w-3/5 flex-col-reverse mb-5">
           <div className="basis-3/5 grow">
-            <h2 className="font-bold text-center md:text-start text-3xl mb-5">Aspiring Front-end Developer</h2>
+            <h2 className="font-bold text-center  md:text-start text-3xl mb-5">
+              Aspiring Front-end Developer
+            </h2>
             <p className="leading-loose">
               My name is Agis Carty and I&apos;m an aspiring front-end developer. From the moment I discovered
               programming, which is an ever-growing topic in{" "}
@@ -24,8 +28,9 @@ export function Bio() {
               >
                 Saint Martin
               </Anchor>
-              , I&apos;ve become dedicated to building <strong>performant</strong> and <strong>accessible</strong>{" "}
-              websites, and hopefully introducing more people to the beauty of modern day web development.
+              , I&apos;ve become dedicated to building <strong>performant</strong> and{" "}
+              <strong>accessible</strong> websites, and hopefully introducing more people to the beauty of
+              modern day web development.
             </p>
           </div>
           <figure className="basis-2/5 grow mb-2 md:mb-0 flex items-start justify-center">
