d621921d1f422bd7f6ae09aa022f08de698134d1
bluepnwage <a.carty2555@gmail.com>
2023-02-16 21:29:34 -0400

    fix styles breaking on mobile

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,7 +10,7 @@ export default function Header() {
       className={`w-full duration-200 ease-out items-center px-4 py-2 lg:py-0 lg:px-0 justify-between 
       flex lg:block lg:w-1/6 lg:space-y-4 border-b lg:border-b-0 border-zinc-300 
       dark:border-zinc-700 bg-background 
-     dark:bg-background-dark sticky lg:fixed top-0 left-0 lg:top-36 lg:left-16`}
+     dark:bg-background-dark sticky lg:fixed top-0 z-[9999] left-0 lg:top-36 lg:left-16`}
     >
       <nav>
         <MobileMenu />
--- components/MDX.tsx
+++ components/MDX.tsx
@@ -7,13 +7,13 @@ export function MDX({ content }: { content: Blog }) {
   const MDXContent = useMDXComponent(content.body.code);
 
   return (
-    <div className="flex flex-col items-center lg:mt-36">
+    <div className="flex flex-col items-center mt-16 lg:mt-36">
       <section
-        className={`w-3/5 space-y-4 mb-20 ${styles.blog}  prose-code:p-1 prose-code:rounded-sm
+        className={`w-4/5 lg:w-3/5 space-y-4 mb-20 prose-pre:overflow-x-scroll ${styles.blog}  prose-code:p-1 prose-code:rounded-sm
      prose-ul:list-disc prose-ul:p-4 prose-pre:rounded-md prose-pre:p-4`}
       >
         <header>
-          <h1 className="display-large text-center mb-5">{content.title}</h1>
+          <h1 className=" text-4xl lg:text-6xl text-center mb-5">{content.title}</h1>
         </header>
         <MDXContent components={{ pre: CodeBlock }} />
       </section>
