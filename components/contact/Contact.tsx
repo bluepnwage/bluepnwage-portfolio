55c114f1dc5083460218a93f40089cf21a7bea8a
bluepnwage <a.carty2555@gmail.com>
2024-07-02 15:07:04 -0400

    update contact section

--- components/contact/Contact.tsx
+++ components/contact/Contact.tsx
@@ -1,25 +1,24 @@
-import type { ReactNode } from "react";
-import Link from "next/link";
-import { ArrowUpRight } from "lucide-react";
+import type { ReactNode } from 'react';
+import Link from 'next/link';
+import { ArrowUpRight } from 'lucide-react';
 
 export function Contact() {
   return (
     <section
-      id={"contact"}
+      id={'contact'}
       className="mb-20 grid lg:grid-cols-2 gap-12"
     >
       <header>
-        <h2 className="font-bold text-3xl mb-5">Contact</h2>
-        <p className="text-gray-200">Corem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <h2 className="font-bold text-3xl mb-5">Let&apos;s get in touch</h2>
       </header>
       <div className="flex flex-col gap-12">
         <Link
-          href={"/"}
+          href={'/'}
           className="bg-neutral-700 lg:w-2/3 lg:ml-auto  rounded flex flex-col justify-center gap-4 p-6"
         >
           <p className="text-gray-100 font-semibold text-xl">Github</p>
           <p className="flex items-center text-gray-200">
-            bluepnwage{" "}
+            bluepnwage{' '}
             <ArrowUpRight
               // color="inherit"
               className="inline-block ml-2 text-gray-300"
@@ -28,11 +27,18 @@ export function Contact() {
           </p>
         </Link>
         <Link
-          href={"/"}
+          href={'/'}
           className="bg-neutral-700 lg:w-2/3 lg:ml-auto  rounded flex flex-col justify-center gap-4 p-6"
         >
           <p className="text-gray-100 font-semibold text-xl">Email</p>
-          <p className="flex items-center text-gray-200">a.carty2555@gmail.com </p>
+          <p className="flex items-center text-gray-200">
+            a.carty2555@gmail.com{' '}
+            <ArrowUpRight
+              // color="inherit"
+              className="inline-block ml-2 text-gray-300"
+              size={16}
+            />{' '}
+          </p>
         </Link>
       </div>
     </section>
@@ -55,7 +61,7 @@ function ContactInfo({ label, contact, icon, href }: PropTypes) {
         <a
           href={href}
           rel="noreferrer"
-          target={"_blank"}
+          target={'_blank'}
           className="font-semibold"
         >
           {contact}
